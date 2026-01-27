20b7a3e257232266f62cc62f0bfbf064a9544c97
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2026-01-27 19:46:22 -0300

    fix

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
 
-app.listen(3000, () => {
+app.listen(3007, () => {
   console.log('SaudavelMente API rodando em http://localhost:3000');
 });
--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const cors = require("cors")
 
 const app = express();
+app.use(cors())
 app.use(express.json());
-
-module.exports = app;
 
 const authController = require('./controllers/auth.controller');
 const therapistController = require('./controllers/therapist.controller');
